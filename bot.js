5cd9a4e57efb7fa52ff9f09affacf7f9b354f668
wolfpro231 <48035876+wolfpro231@users.noreply.github.com>
2019-06-08 23:06:10 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -25,7 +25,7 @@ client.on('message', message => {
       message.channel.send(`**تـــ تغيير الحالة ــم   ${argresult}**`)
   } else 
   if (message.content.startsWith(adminprefix + 'Lst')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/dream");
+    client.user.setGame(argresult, "https://www.twitch.tv/wolfgod1224");
       message.channel.send(`**تـــ تغيير الحالة ــم**`)
   }
   if (message.content.startsWith(adminprefix + 'setname')) {
